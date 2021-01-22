37cb13b69c5afea3d3d04d89717a55c4ddc0b073
bernardo-trevizan <bernardo.trevizan@bluemetrics.com.br>
2021-01-22 11:04:04 -0300

    Improve evaluation performance

--- scripts/evaluation.R
+++ scripts/evaluation.R
@@ -28,21 +28,41 @@ if(file.exists(results_path))
   print("Build dataset.")
   data <- dataset.load(datapath)
   
-  for(t in thresholds) {
-    for(m in methods) {
-      for(b in n_bags_vec) {
-        for(a in names(aggrs)) {
-          aggr <- list()
-          aggr[[a]] <- aggrs[[a]]
+  partitions <- ensemble.data_partition(data$x, data$y, train_perc)
+  
+  train_i <- partitions[["train"]]
+  train_x <- data$x[train_i, ]
+  train_y <- data$y[train_i]
+  
+  val_i <- partitions[["validation"]]
+  validation_x <- data$x[val_i, ]
+  validation_y <- data$y[val_i]
+  
+  for(b in n_bags_vec) {
+    
+    bags <- ensemble.create_bags(train_x, train_y, b)
+    
+    for(a in names(aggrs)) {
+      
+      aggr <- list()
+      aggr[[a]] <- aggrs[[a]]
+      
+      for(f in names(fses)) {
+      
+        fs <- list()
+        fs[[f]] <- fses[[f]]
+        
+        print("============================= Evaluate ensemble =============================")
+        fs_result <- ensemble.feature_selection(fs, aggr, bags)
+      
+        
+        for(t in thresholds) {
           
-          for(f in names(fses)) {
+          for(m in methods) {
+            
             result <- results %>% filter(Threshold == t, Classifier == m, Number.of.bags == b, Aggregation.method == a, Feature.selector == f)
             if(nrow(result) > 0) next;
             
-            fs <- list()
-            fs[[f]] <- fses[[f]]
-            
-            print("============================= Evaluate ensemble =============================")
             print(paste('Dataset = ', filename))
             print(paste('Threshold = ', t))
             print(paste('Method = ', m))
@@ -50,9 +70,22 @@ if(file.exists(results_path))
             print(paste('Aggregation = ', a))
             print(paste('Feature Selection = ', f))
             
-            r <- ensemble.eval(data$x, data$y, b, fs, aggr, t, m, train_perc, tr_control)
-            r <- cbind(r$results, list('Dataset' = filename))
-       
+            r <- ensemble.eval(validation_x, validation_y, fs_result$rankings, fs_result$final_ranking, t, m, tr_control)
+            
+            r[['Number of bags']] <- c(r[['Number of bags']], b)
+            r[['Feature selector']] <- c(r[['Feature selector']], f)
+            r[['Aggregation method']] <- c(r[['Aggregation method']], a)
+            
+            r <- data.frame(r)
+            r$Number.of.bags <- as.factor(r$Number.of.bags)
+            r$Feature.selector <- as.factor(r$Feature.selector)
+            r$Aggregation.method <- as.factor(r$Aggregation.method)
+            r$Threshold <- as.factor(r$Threshold)
+            r$Classifier <- as.factor(r$Classifier)
+            r$K.Fold <- as.factor(r$K.Fold)
+            r$Evaluation.method <- as.factor(r$Evaluation.method)
+            
+            r <- cbind(r, list('Dataset' = filename))
             results <- rbind(results, r)
             
             print(paste('Mean Recall =', r$Recall.Mean))
--- scripts/utils/ensemble.R
+++ scripts/utils/ensemble.R
@@ -2,74 +2,17 @@ source("scripts/utils/metrics.R")
 source("scripts/utils/dataset.R")
 source("scripts/utils/train.R")
 
-ensemble.eval <- function(x, y, n_bags, fs, aggr, threshold, method, train_perc = 0.6, tr_control = get_repeated_cv()) {
-  classes <- summary(y)
-  
-  fs_name <- names(fs)[1]
-  fs <- fs[[fs_name]]
-  
-  aggr_name <- names(aggr)[1]
-  aggr <- aggr[[aggr_name]]
-  
-  # 10-fold cross validation ####################################################
-  results <- list()
-  k <- 1
-  
-  train_i <- createDataPartition(y, times = 1, p = train_perc, list = TRUE)[[1]]
-  validation_i <- setdiff(1:nrow(x), train_i)
-  
-  #### 70% for training
-  train_x <- x[train_i, ]
-  train_y <- y[train_i]
-  
-  ##### For each bag
-  i <- 1
-  rankings <- list()
-  
-  for(bag in 1:n_bags) {
-    print(paste("Run bag #", i, " of ", n_bags, " bags.", sep = ""))
-    
-    # Resample bag with at least 3 normal observations
-    n_normal <- 0
-    n_tumor <- 0
-    
-    while((n_normal < 4) | (n_tumor < 4)) {
-      indices <- createResample(train_y, times = 1, list = TRUE)[[1]]
-      bag_y <- train_y[indices]
-      
-      n_normal <- length(bag_y[bag_y == 'Normal'])
-      n_tumor <- length(bag_y[bag_y == 'Tumor'])
-    }
-    
-    # Apply SMOTE over bag
-    bag <- dataset.smote(train_x[indices, ], bag_y, 3)
-    bag_x <- bag$x
-    bag_y <- bag$y
-    
-    ###### Run feature selection
-    ranking <- fs(bag_x, bag_y)
-    
-    ###### Save feature ranking
-    rankings[[i]] <- ranking
-    
-    i <- i + 1
-  }
-  
+ensemble.eval <- function(validation_x, validation_y, rankings, final_ranking, threshold, method, tr_control) {
   ##### Calculate stability
   print("Calculate stability.")
-  stability <- stability_index(lapply(rankings, function(r) { head(r$feature, n = threshold) }), threshold, ncol(x))
+  stability <- stability_index(lapply(rankings, function(r) { head(r$feature, n = threshold) }), threshold, nrow(final_ranking))
   print(stability$stability)
-  
-  ##### Aggregate rankings
-  print("Aggregate rankings.")
-  final_ranking <- aggr(rankings)
   
   ##### Apply threshold
   selected_genes <- head(final_ranking$feature, n=threshold)
   
   #### 30% for validation
-  validation_x <- x[validation_i, selected_genes]
-  validation_y <- y[validation_i]
+  validation_x <- validation_x[, selected_genes]
   
   ##### Train classifier ####
   print(paste("Train model with method ", method, ".", sep = ""))
@@ -93,9 +36,7 @@ ensemble.eval <- function(x, y, n_bags, fs, aggr, threshold, method, train_perc 
   }
   
   #### Save results ####
-  results[['Number of bags']] <- c(results[['Number of bags']], n_bags)
-  results[['Feature selector']] <- c(results[['Feature selector']], fs_name)
-  results[['Aggregation method']] <- c(results[['Aggregation method']], aggr_name)
+  results <- list()
   results[['Threshold']] <- c(results[['Threshold']], threshold)
   results[['Classifier']] <- c(results[['Classifier']], method)
   results[['K-Fold']] <- c(results[['K-Fold']], kfold)
@@ -103,19 +44,67 @@ ensemble.eval <- function(x, y, n_bags, fs, aggr, threshold, method, train_perc 
   results[['Evaluation method']] <- c(results[['Evaluation method']], tr_control$method)
   results[['Stability']] <- c(results[['Stability']], stability$stability)
   for(metric in names(sum_metrics)) results[[metric]] <- c(results[[metric]], sum_metrics[[metric]])
-
-  results <- data.frame(results)
-  results$Number.of.bags <- as.factor(results$Number.of.bags)
-  results$Feature.selector <- as.factor(results$Feature.selector)
-  results$Aggregation.method <- as.factor(results$Aggregation.method)
-  results$Threshold <- as.factor(results$Threshold)
-  results$Classifier <- as.factor(results$Classifier)
-  results$K.Fold <- as.factor(results$K.Fold)
-  results$Evaluation.method <- as.factor(results$Evaluation.method)
   
   #### Finish ####
   print("Done.")
+  return(results)
+}
+
+ensemble.create_bags <- function(x, y, n_bags) {
+  bags <- list()
+  i <- 1
   
-  res_list <- list("stability" = stability, "final_ranking" = final_ranking, "rankings" = rankings, "results" = results)
-  return(res_list)
+  for(bag in 1:n_bags) {
+    # Resample bag with at least 3 normal observations
+    n_normal <- 0
+    n_tumor <- 0
+    
+    while((n_normal < 4) | (n_tumor < 4)) {
+      indices <- createResample(y, times = 1, list = TRUE)[[1]]
+      bag_y <- train_y[indices]
+      
+      n_normal <- length(bag_y[bag_y == 'Normal'])
+      n_tumor <- length(bag_y[bag_y == 'Tumor'])
+    }
+    
+    # Apply SMOTE over bag
+    bags[[i]] <- dataset.smote(x[indices, ], bag_y, 3)
+    i <- i + 1
+  }
+  
+  return(bags)
+}
+
+ensemble.data_partition <- function(x, y, train_perc) {
+  train_i <- createDataPartition(y, times = 1, p = train_perc, list = TRUE)[[1]]
+  validation_i <- setdiff(1:nrow(x), train_i)
+  
+  result <- list("train" = train_i, "validation" = validation_i)
+  return(result)
+}
+
+ensemble.feature_selection <- function(fs, aggr, bags) {
+  fs_name <- names(fs)[1]
+  fs <- fs[[fs_name]]
+  
+  aggr_name <- names(aggr)[1]
+  aggr <- aggr[[aggr_name]]
+  
+  ##### Feature selection step
+  i <- 1
+  n_bags <- length(bags)
+  rankings <- list()
+  
+  for(bag in bags) {
+    print(paste("Run bag #", i, " of ", n_bags, " bags.", sep = ""))
+    rankings[[i]] <- fs(bag$x, bag$y)
+    i <- i + 1
+  }
+  
+  ##### Aggregate rankings
+  print("Aggregate rankings.")
+  final_ranking <- aggr(rankings)
+  
+  result <- list("rankings" = rankings, "final_ranking" = final_ranking)
+  return(result)
 }
